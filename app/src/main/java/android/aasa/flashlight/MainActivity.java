58f31c5c685fe5f46ed61c985b9edc95a4bd7a96
arpit0303 <arpitagrawal910@gmail.com>
2016-09-28 22:09:29 +0530

    Adding multi-screen support

--- app/src/main/java/android/aasa/flashlight/MainActivity.java
+++ app/src/main/java/android/aasa/flashlight/MainActivity.java
@@ -4,70 +4,98 @@ import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.ToggleButton;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
+import com.startapp.android.publish.StartAppAd;
+import com.startapp.android.publish.StartAppSDK;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     Camera camera;
 
     Camera.Parameters parameters;
     ToggleButton mFlashSwitch;
-    WindowManager.LayoutParams localLayoutParams;
-    Boolean isFlashLightOn = true;
+    Boolean checkFlash;
+    static int click = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        StartAppSDK.init(this, "203511945", false);
+        StartAppAd.disableSplash();
+
         mFlashSwitch = (ToggleButton) findViewById(R.id.toggleButton);
 
-        localLayoutParams = getWindow().getAttributes();
+        checkFlash = this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
-        AdView mAdView = (AdView) findViewById(R.id.adView);
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
-
-        Boolean checkFlash = this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-
-        if (checkFlash) {
-            cameraController();
-
-            mFlashSwitch.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    cameraController();
-                }
-            });
-        }
+        mFlashSwitch.setOnClickListener(this);
     }
 
-    private void cameraController() {
-        if (mFlashSwitch.isChecked()) {
-            camera = Camera.open();
-            parameters = camera.getParameters();
-            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-            camera.setParameters(parameters);
-            camera.startPreview();
-
-            mFlashSwitch.setChecked(false);
-        } else {
-            camera.stopPreview();
-
-            mFlashSwitch.setChecked(true);
-        }
-    }
-
-    private void releaseCameraAndPreview() {
+    public void releaseCameraAndPreview() {
         if (camera != null) {
             camera.release();
             camera = null;
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+                parameters = camera.getParameters();
+            } catch (RuntimeException e) {
+                Log.e("Error: ", e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseCameraAndPreview();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.toggleButton:
+                if (checkFlash) {
+                    click++;
+                    if (click % 2 == 0) {
+                        if (camera != null || parameters != null) {
+                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                            camera.setParameters(parameters);
+                            camera.startPreview();
+                        }
+
+                        mFlashSwitch.setChecked(false);
+                    } else {
+                        if (camera != null || parameters != null) {
+                            parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+                            camera.setParameters(parameters);
+                            camera.stopPreview();
+                        }
+
+                        mFlashSwitch.setChecked(true);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        StartAppAd.onBackPressed(this);
+        super.onBackPressed();
     }
 }
